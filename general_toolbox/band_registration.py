3f601d052be5feefd2cf2f58f52e5395afebd993
Ryan Hartzell <rah3156@collins.cis.rit.edu>
2017-09-14 12:09:39 -0400

    added a general toolbox and an empty registration tool

---
+++ general_toolbox/band_registration.py
@@ -0,0 +1,9 @@
+'''
+This file should contain various functions for registration of multiple bands
+of the same frame #. Try multiple registration methods, starting with SIFT.
+
+
+'''
+
+import cv2
+import numpy as np
